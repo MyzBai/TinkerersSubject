d2e9c309708f7a231ca35e9355ce67a1a5243e66
MyzBai <myzzie@protonmail.com>
2022-12-08 18:01:06 +0100

    make components hidden by default

--- src/game/components/loader.ts
+++ src/game/components/loader.ts
@@ -3,23 +3,12 @@ import { init as initAchievements } from './achievements';
 import type GConfig from '@src/types/gconfig';
 
 export type ComponentNames = keyof Pick<GConfig, 'items' | 'achievements'>;
-type Properties = Partial<Pick<GConfig, ComponentNames>>;
 
-
-export default function load(components: Properties) {
-    initComponent('items', components.items, initItems);
-    initComponent('achievements', components.achievements, initAchievements);
-}
-
-function initComponent(name: string, data: Properties[ComponentNames], initFunc: (params: Properties[ComponentNames]) => void){
-    
-    if(data){
-        if(data.levelReq > 1){
-            document.querySelector(`.p-game > menu [data-tab-target="${name}"]`).classList.add('hidden');
-        }
-        initFunc(data);
-    } else {
-        document.querySelector(`.p-game > menu [data-tab-target="${name}"]`).classList.add('hide');
-        document.querySelector(`.p-game .p-${name}`).remove();
+export default function load(components: Pick<GConfig, ComponentNames>) {
+    if (components.items) {
+        initItems(components.items);
+    }
+    if (components.achievements) {
+        initAchievements(components.achievements);
     }
 }
