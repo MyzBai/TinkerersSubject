2298c0ef75e183028f0b290b7b5771492bd4525c
MyzBai <myzzie@protonmail.com>
2022-12-09 09:40:09 +0100

    remove bounty traces

--- src/game/components/loader.ts
+++ src/game/components/loader.ts
@@ -1,16 +1,14 @@
 import { init as initItems } from './items/items';
 import { init as initAchievements } from './achievements';
-import { init as initBounty } from './bounty';
 import type GConfig from '@src/types/gconfig';
 
-export type ComponentNames = keyof Pick<GConfig, 'items' | 'achievements' | 'bounty'>;
+export type ComponentNames = keyof Pick<GConfig, 'items' | 'achievements'>;
 
 export default function load(components: Pick<GConfig, ComponentNames>) {
     for (const key of Object.keys(components)) {
         switch(key as ComponentNames){
             case 'items': initItems(components.items); break;
             case 'achievements': initAchievements(components.achievements); break;
-            case 'bounty': initBounty(components.bounty); break;
         }
     }
 }
