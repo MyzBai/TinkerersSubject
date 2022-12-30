32beba92d6efd9f87a942efe305360e8774d35e1
MyzBai <myzzie@protonmail.com>
2022-12-30 06:56:02 +0100

    return observer

--- src/utils/Observers.ts
+++ src/utils/Observers.ts
@@ -1,10 +1,10 @@
-export const visibilityObserver = (element: HTMLElement, callback: (visible: boolean) => void) => {
+export const visibilityObserver = (element: HTMLElement, callback: (visible: boolean, observer: IntersectionObserver) => void) => {
     const observer = new IntersectionObserver(observe);
     observer.observe(element);
     return observer;
-    function observe(entries: IntersectionObserverEntry[]){
+    function observe(entries: IntersectionObserverEntry[]) {
         entries.forEach(entry => {
-            callback(entry.isIntersecting);
+            callback(entry.isIntersecting, observer);
         });
     }
 }
