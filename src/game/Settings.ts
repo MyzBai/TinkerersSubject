c875953f1b1b17f94bc3785cf24ebba25cd2077f
MyzBai <myzzie@protonmail.com>
2023-02-19 08:24:50 +0100

    always show delete save button

--- src/game/Settings.ts
+++ src/game/Settings.ts
@@ -1,25 +1,18 @@
 import { queryHTML } from "@src/utils/helpers";
 import { GenericModal } from "@src/webComponents/GenericModal";
 import type Game from "./Game";
-import { visibilityObserver } from "@src/utils/Observers";
 
 export default class Settings {
     private readonly deleteSaveButton = queryHTML<HTMLButtonElement>('.p-settings [data-delete-save]');
     constructor(readonly game: Game) {
 
-        visibilityObserver(queryHTML('.p-game .p-settings'), async visible => {
-            if (visible) {
-                const hasSave = await this.game.hasSave(this.game.config.meta.id);
-                this.deleteSaveButton.classList.toggle('hidden', !hasSave);
-            }
-        });
         this.deleteSaveButton.addEventListener('click', this.openDeleteSaveModal.bind(this));
 
     }
 
-    openDeleteSaveModal() {
+
+    private openDeleteSaveModal() {
         const modal = queryHTML<GenericModal>('generic-modal');
-        console.log(modal);
         modal.init({
             title: 'Delete Save',
             body: 'Are you sure?',
@@ -29,7 +22,6 @@ export default class Settings {
                 if (confirm) {
                     await this.game.deleteSave();
                     //dispose game and return to home
-                    this.game.dispose();
                     this.game.home.init();
                 }
             }
