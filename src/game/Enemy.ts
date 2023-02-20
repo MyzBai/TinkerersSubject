763b44f4e042fd2987ec4875f05703edd82511bb
MyzBai <myzzie@protonmail.com>
2023-02-20 12:30:32 +0100

    refactor

--- src/game/Enemy.ts
+++ src/game/Enemy.ts
@@ -31,10 +31,6 @@ export default class Enemy {
     init() {
         this.game.onSave.listen(this.save.bind(this));
 
-        this.onDeath.listen(() => {
-            this._index++;
-        });
-
         this.healthList = this.game.config.enemies.enemyList;
         this._index = this.game.saveObj.enemy?.index || 0;
         this.health = this.game.saveObj.enemy?.health || this.maxHealth;
@@ -57,6 +53,7 @@ export default class Enemy {
 
         if (this.health <= 0) {
             this.health = 0;
+            this._index++;
             this.onDeath.invoke(this);
             this.spawn();
         }
