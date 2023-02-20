d9e3cdd48787727c58dd9bec9d29172ecf25cade
MyzBai <myzzie@protonmail.com>
2023-02-20 15:43:51 +0100

    use .schema filename extension

--- src/utils/validateConfig.ts
+++ src/utils/validateConfig.ts
@@ -1,5 +1,4 @@
 import Ajv from 'ajv';
-import schema from '@public/gconfig/configSchema.json';
-
+import schema from '@public/gconfig/config.schema.json';
 
 export const validateConfig = new Ajv().compile(schema);
